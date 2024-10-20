6cf6d3c0c19f7ae2bb7d2ba1f82682a3a49a6ced
YahyaYigitYildiz <58yyy58a@gmail.com>
2024-10-20 20:31:12 +0300

    Arrays

---
+++ main.rs
@@ -0,0 +1,52 @@
+
+
+fn main() {
+
+    
+
+    
+
+
+    let mut dizi =vec![12,13,14];
+    dizi.push(16);
+
+
+
+    for number in dizi {
+
+        let mut toplam = 0;
+
+        toplam = number + toplam ;
+
+        println!("{}",toplam);
+    }
+    
+
+
+}
+
+/* 
+fn calculat(sayi:i32,sayi2:i32,değer:i32) {
+
+    if değer ==1  {
+
+        println!("Toplama {}",sayi+sayi2)
+         
+    }
+    else if değer==2 {
+
+        println!("çıkarma {}",sayi-sayi2)
+        
+    }
+    else {
+        println!("sayı geçersiz")
+    }
+}
+    */
+
+
+
+
+
+
+
